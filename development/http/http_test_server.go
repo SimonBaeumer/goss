617d4b6cc87e98058ad64bee5ee7de71b83b65c9
Simon Bäumer <simon.baeumer@sva.de>
2019-02-22 14:42:59 +0100

    Add redirect route to http_test_server

--- development/http/http_test_server.go
+++ development/http/http_test_server.go
@@ -6,21 +6,37 @@ import (
 	"net/http"
 )
 
+const RedirectStatusCode = 302
+
 func httpHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("/")
+
+	printHeader(r)
+
+	w.Header().Add("key", "value")
+}
+
+func printHeader(r *http.Request) {
 	for k, values := range r.Header {
 		for _, value := range values {
 			fmt.Println("key:", k, "val:", value)
 		}
 	}
-
-	w.Header().Add("key", "value")
 }
 
 func main() {
+	fmt.Println("Running on localhost:9090")
 	http.HandleFunc("/", httpHandler)
+	http.HandleFunc("/redirect", redirectHandler)
 	err := http.ListenAndServe(":9090", nil)
 
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
+}
+
+func redirectHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("/redirect")
+	printHeader(r)
+	http.Redirect(w, r, "/", RedirectStatusCode)
 }
